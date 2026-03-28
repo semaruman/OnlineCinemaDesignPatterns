31e8fd54009f4be200f9f765c6f69b8dafdc119b
Semen <semen.ruman@gmail.com>
2026-03-28 11:45:09 +0300

    Начал делать минимал api: добавил меню

--- OnlineCinemaWebApi/Program.cs
+++ OnlineCinemaWebApi/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", IndexMethod);
 
 app.Run();
+
+
+IResult IndexMethod()
+{
+    string menu = @"
+GET: /serials - получение списка сериалов
+GET: /serials/id - получение информации о конкретном сериале
+GET: /serials/id/subscribers - получение подписчиков сириала
+";
+    var data = new {
+        message = menu
+    };
+
+    return Results.Ok(data);
+}
